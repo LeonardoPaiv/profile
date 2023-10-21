02a2442edff176511bf706d5c2d447f1c01ff580
LeonardoPaiv <leopaiva45@gmail.com>
2023-10-21 12:10:45 -0300

    testando animações na barra de XP

--- src/components/XpBar.tsx
+++ src/components/XpBar.tsx
@@ -1,7 +1,7 @@
 const XpBar = ({w}:{w: number}) => {
   return (
-    <div className="w-128 h-14 m-3 border border-orange shadow-lg rounded-full p-2">
-      <div style={{width: `${w}%`}} className="h-full bg-purple shadow-inner rounded-full"></div>
+    <div className="w-full h-14 m-3 border border-orange shadow-lg rounded-full p-2">
+      <div style={{width: `${w}%`}} className="h-full xpBar shadow-inner rounded-full"></div>
     </div>
   );
 };
